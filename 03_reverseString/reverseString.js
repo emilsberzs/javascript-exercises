c1837140b07cdbb9c51aea1ce7a0e6cb28792a6d
Emils <emilsberzs@hotmail.com>
2022-12-16 13:19:57 +0000

    Reverse string tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let reversed_array = []
+for (let i = 0; i < string.length; i++ ){
+    reversed_array.unshift(string[i]);
+}
+let reversed_string = reversed_array.join('')
+return reversed_string
 };
 
 // Do not edit below this line
