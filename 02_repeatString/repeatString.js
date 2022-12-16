1c6b39c23980f63f9d90c91d519897140016f6a1
Emils <emilsberzs@hotmail.com>
2022-12-16 13:05:11 +0000

    All repeat_string tests pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times >= 1){
+    return string.repeat(times);
+    } else if (times < 0) {
+        return "ERROR"
+    } else {
+        return ""
+    }
 };
 
 // Do not edit below this line
