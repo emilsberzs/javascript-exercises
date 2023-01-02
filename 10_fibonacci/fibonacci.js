67d4c88a562dc3134994f7d481aea37fc7d99603
Emils <emilsberzs@hotmail.com>
2023-01-02 11:44:05 +0000

    Fibonacci sequqnce with While loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+let n1 = 0;
+let n2 = 1;
+let nextNumber = n1 + n2
+let sequence = [0,1,1,]
+const fibonacci = function (number) {
+    if (number < 0){
+        return "OOPS"
+    } else {
+    while (sequence.length <= number){
+        console.log(nextNumber);
+        n1 = n2;
+        n2 = nextNumber;
+        nextNumber = n1 + n2;
+        sequence.push(nextNumber)
 
+    }
+    console.log(sequence[number])
+    return sequence[number]
+}
 };
 
 // Do not edit below this line
