0b2875f8cae1bd6aba41ecfc2e368fd7492e7eef
Emils <emilsberzs@hotmail.com>
2023-01-08 11:41:59 +0000

    Backdated commit for chapter 12 of JavaScript exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+let oldest_age = 0
+let oldest_name = []
+for (let i = 0; i < people.length; i++){
+ if (people[i].yearOfDeath - people[i].yearOfBirth > oldest_age) {
+    oldest_age = people[i].yearOfDeath - people[i].yearOfBirth
+    oldest_name = people[i]
+ }
+}
+console.log(oldest_name, oldest_age)
+return oldest_name
 };
 
 // Do not edit below this line
